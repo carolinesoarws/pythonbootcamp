6f48b51f547edcb9e3ab94b120d3bc8b3e33a0b6
cssfqbx <cssfqbx@Carolines-MacBook-Pro.local>
2024-06-28 19:41:40 -0300

    python bootcampo day 01

---
+++ day01-code.py
@@ -0,0 +1,29 @@
+print("Hello World!")
+
+# Exercicses 01
+print("1. Mix 500g of Flour, 10g Yest and 300ml water iin a bowl.")
+
+
+# String Manipulation and Code Intelligence
+
+# 01 - Line Break
+print("Hello World!\nHello World!")
+
+# 02 - String concatenation
+print("Hello World!" + "Carol")
+print("Hello World!" + " " + "Carol")
+
+# The Python Input Function
+input("What is your name?")
+
+# Python Variables
+
+name = "Jack"
+print(name)
+
+print("Rose")
+print(name)
+
+name = input("What's your name?")
+length = len(name)
+print(length)
